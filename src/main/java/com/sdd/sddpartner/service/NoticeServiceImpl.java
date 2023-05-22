ad49ed9f03f930c2eb6b0eeefaf0c0aeea7f4068
chou <asha.jisu@gmail.com>
2023-05-22 20:40:43 +0900

    Notice FEAT searchlist(), threelist() 검색 titleContaning, 메인 Top3by

--- src/main/java/com/sdd/sddpartner/service/NoticeServiceImpl.java
+++ src/main/java/com/sdd/sddpartner/service/NoticeServiceImpl.java
@@ -1,16 +1,12 @@
 package com.sdd.sddpartner.service;
 
 import java.util.List;
-import java.util.Optional;
 
-import com.sdd.sddpartner.domain.Employee;
 import com.sdd.sddpartner.domain.Notice;
 import com.sdd.sddpartner.domain.User;
-import com.sdd.sddpartner.repository.EmployeeRepository;
 import com.sdd.sddpartner.repository.NoticeRepository;
 import com.sdd.sddpartner.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.weaver.ast.Not;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
@@ -58,5 +54,14 @@ public class NoticeServiceImpl implements NoticeService {
 	public List<Notice> list() throws Exception {
 		return repository.findAll(Sort.by(Direction.DESC, "noticeNo"));
 	}
-	
+	@Override
+	public List<Notice> threelist() throws Exception {
+		return repository.findTop3By(Sort.by(Direction.DESC, "noticeNo"));
+	}
+
+	@Override
+	public List<Notice> searchlist(String title) throws Exception {
+		return repository.findByTitleContaining(Sort.by(Direction.DESC, "noticeNo"),title);
+	}
+
 }
